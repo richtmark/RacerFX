d65a65d984bf2fdc1c8c9b9b3f0d3ed49086020d
Robert <robert.hoeger@googlemail.com>
2017-01-25 02:27:30 +0100

    Prototyp Parralax implemenitiert

--- RacerFx/src/application/Screen2Controller.java
+++ RacerFx/src/application/Screen2Controller.java
@@ -18,7 +18,9 @@ import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.animation.PathTransition.OrientationType;
 import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -88,6 +90,8 @@ public class Screen2Controller implements Initializable , ControlledScreen {
     private ImageView idBackgroundImageView; 
     @FXML
     private Canvas idCanvas;
+    @FXML 
+    private ImageView idimagecar;
 
     
     private static final Integer KEYSTARTTIMECOUNTDOWN = 10; //Countdown Eingabe sek
@@ -96,21 +100,22 @@ public class Screen2Controller implements Initializable , ControlledScreen {
     private Timeline timelineQuizCountdown; //Timeline fuer das Quiz
     private IntegerProperty propertyKeySecondsCountdown = new SimpleIntegerProperty(KEYSTARTTIMECOUNTDOWN);
     private IntegerProperty propertyQuizSecondsCountdown = new SimpleIntegerProperty(QUIZSTARTTIMECOUNTDOWN);
+    private DoubleProperty propertyYValueBackground;
     private QuizRace quizRaceObj;
     private KeyRace keyRaceObj;
-    private QuestionModel questionObjekt;
-    //private StringProperty inputStringProperty;
-    private double megai=0;
+    private QuestionModel questionObjekt;    
+    private double stop;
+    private double speedduration;
+    private PathTransition anim;
 
-        
-    private GraphicsContext gc;
         
 
     /**
      * Initializieren der Controllerklasse
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {       	
+    public void initialize(URL url, ResourceBundle rb) {   
+    	speedduration = 0;
     	idRadioButtonAnswer1.setUserData("1"); //value zuweisen nicht da auf die schnelle nicht gefunden in FXML...
     	idRadioButtonAnswer2.setUserData("2");
     	idRadioButtonAnswer3.setUserData("3");
@@ -151,16 +156,6 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                 
                 
                 //#######################Timeline Countdown Key##################################
-                /*
-                 * 
-                 * Die Timeline fuer den Countdown Key steuern
-                 * 
-                 */
-                /*
-                if (timelineKeyCountdown != null) {
-                    timelineKeyCountdown.stop();
-                }
-                */
                 propertyKeySecondsCountdown.set(KEYSTARTTIMECOUNTDOWN);
                 timelineKeyCountdown = new Timeline();
                 timelineKeyCountdown.getKeyFrames().add(
@@ -207,12 +202,21 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                     public void changed(ObservableValue<? extends Toggle> ov,
                         Toggle old_toggle, Toggle new_toggle) {
                             if (answerToggleGroup.getSelectedToggle() != null) {
+                            	System.out.println("Toggle");
                             	//pruefe Antwort
                             	if (answerToggleGroup.getSelectedToggle().getUserData().equals(questionObjekt.getTrueAnswerString())) {
-                            		System.out.println("Answert true");
+                            		System.out.println("Answert true Quiz");
+                            		speedduration = speedduration+1;
+                            		anim.setRate(speedduration);
+
+                            	} else {
+                            		System.out.println("Answert false Quiz");                            		
+                            		speedduration = speedduration-1;
+                            		anim.setRate(speedduration);
                             	}
                             	//System.out.println(answerToggleGroup.getSelectedToggle().getUserData().toString());
-                            	answerToggleGroup.getSelectedToggle().setSelected(false);                            	
+                            	answerToggleGroup.getSelectedToggle().setSelected(false); 
+                            	idTxfInput.requestFocus();
                             	//group.getSelectedToggle().getUserData().toString();
                             	questionObjekt = quizRaceObj.getRandomQuestion(); //neues QuestionModel anfragen 
                             	idTxtAreaQuestion.setText(questionObjekt.getQuestiontext());
@@ -238,7 +242,7 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                 
                 //################## Listener auf Textfield  Keyeingabe (noch einfacher als unten) + Timelinereset via neue Timeline ###########                
                 idTxfInput.textProperty().addListener((observable, oldValue, newValue) -> {
-                    System.out.println("textfield changed from " + oldValue + " to " + newValue);
+                    //System.out.println("textfield changed from " + oldValue + " to " + newValue);
                     if(idTxfInput.getText().length() == 4) {
                     	String tempInput = idTxfInput.getText();  
                     	//idTxfInput.clear(); 
@@ -248,8 +252,11 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                         	idTxfInput.clear(); 
                         });         
                     	boolean tempBool = keyRaceObj.isInputCorrect(tempInput);
-                    	System.out.println("Input String ist korrekt: "  + tempBool);   
+                    	  
                     	if (tempBool == true) {
+                    		System.out.println("KeyString korrekt: "  + tempBool); 
+                    		speedduration = speedduration+1;
+                    		anim.setRate(speedduration);
                     		idTxfWanted.setText(keyRaceObj.getNewRequestedString());   
                     		timelineKeyCountdown.stop();
                             propertyKeySecondsCountdown.set(KEYSTARTTIMECOUNTDOWN);
@@ -260,7 +267,12 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                             timelineKeyCountdown.setCycleCount(Timeline.INDEFINITE); //ewig wiederholen
                             //timeline.cycleCountProperty().set(5); // 5 mal wiederholen
                             timelineKeyCountdown.playFromStart(); 
-                    	}                    	
+                    	} else {
+                    		//ToDo speed manipulieren
+                    		System.out.println("KeyString false: "  + tempBool); 
+                    		speedduration = speedduration-1;
+                    		anim.setRate(speedduration);
+                    	}
                     	//timelineKeyCountdown.playFromStart(); 
                     }
                 });                
@@ -278,8 +290,6 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                     @Override
                     public void changed(ObservableValue observableValue, Object oldValue, Object newValue) {
                       //System.out.println("oldValue:"+ oldValue + ", newValue = " + newValue);
-                    	megai = megai+10;
-                    	idBackgroundImageView.translateXProperty().set(megai);
                       if ((int)newValue == 0) {
                     	  //Anfrage neue Tastenkombination
                     	  idTxfWanted.setText(keyRaceObj.getNewRequestedString());
@@ -302,7 +312,7 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                 final ChangeListener changeListenerCountdownQuiz = new ChangeListener() {
                     @Override
                     public void changed(ObservableValue observableValue, Object oldValue, Object newValue) {
-                      //System.out.println("oldValue:"+ oldValue + ", newValue = " + newValue);
+                    	//System.out.println("oldValue:"+ oldValue + ", newValue = " + newValue);
                     	if ((int)newValue == 0) {
 	                    	  //Anfrage neue Tastenkombination
 	                      	questionObjekt = quizRaceObj.getRandomQuestion(); //neues QuestionModel anfragen 
@@ -310,7 +320,7 @@ public class Screen2Controller implements Initializable , ControlledScreen {
 	                    	idRadioButtonAnswer1.setText(questionObjekt.getAnswerOne());
 	                    	idRadioButtonAnswer2.setText(questionObjekt.getAnswerTwo());
 	                    	idRadioButtonAnswer3.setText(questionObjekt.getAnswerThree());
-	                    	idRadioButtonAnswer4.setText(questionObjekt.getAnswerFour());
+	                    	idRadioButtonAnswer4.setText(questionObjekt.getAnswerFour());                    		
                       }
                     }	
                 };                
@@ -322,12 +332,56 @@ public class Screen2Controller implements Initializable , ControlledScreen {
                                              
             }
         });
-    	iv.setX(10); //Imageview
-    	//gc = gamecanvas.getGraphicsContext2D();	//die Canvas    
     	
-    	
-    	
-    	
+    	//####################################Rollbild ###############################	
+
+
+        PathElement[] path = 
+        {
+        	new MoveTo(300, -8790), //bild bei 300, -8850 im Container
+            new LineTo(300, 10000), // bild ende oben bei 300, 9000 container
+            new ClosePath()
+        };        
+        
+        //System.out.println(idBackgroundImageView.boundsInParentProperty()); alle
+        
+        Path road = new Path();
+        road.getElements().addAll(path);
+        anim = new PathTransition();
+        anim.setNode(idBackgroundImageView);  
+        stop = 17700;
+        propertyYValueBackground = new SimpleDoubleProperty();
+        propertyYValueBackground.bind(idBackgroundImageView.translateYProperty());
+        propertyYValueBackground.addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue ov, Object t, Object t1) {
+               //System.out.println((double) t1);
+            	System.out.println(speedduration);
+               if (idBackgroundImageView.translateYProperty().doubleValue() > stop) {
+            	   anim.pause();
+               }
+            }
+        }); 
+        
+        
+        
+        
+        anim.setPath(road);
+        anim.setOrientation(OrientationType.NONE);
+        anim.setInterpolator(Interpolator.LINEAR);
+        anim.setDuration(new Duration(400000)); //speed hier je höher desto langsamer
+        anim.setCycleCount(Timeline.INDEFINITE);  
+        anim.setRate(speedduration);
+        playbutton.setOnMouseClicked(me -> 
+                    {
+                      Animation.Status status = anim.getStatus();
+                      if (status == Animation.Status.RUNNING &&
+                          status != Animation.Status.PAUSED)
+                          anim.pause();
+                      else
+                          anim.play();     
+                    });
+        
     }
     
     //#########################################################
